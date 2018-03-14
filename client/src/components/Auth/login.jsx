60f69329fa172fb45c87da48cef997deeaa79660
downier <raulishere@gmail.com>
2018-03-13 23:42:39 -0700

    [auth] Framework for login setup

---
+++ client/src/components/Auth/login.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      password: '',
+      username: ''
+    }
+  }
+  render() {
+    return(
+      <div>heyo</div>
+    )
+  }
+}
+
+export default Login;
