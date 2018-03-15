67e4c49ea15db4ff0ceb2d4ebe267fdfb1f28b2f
Alex Jaynes <dr.ocheyo@gmail.com>
2018-03-15 14:27:32 -0700

    findMoves now returns valid moves

--- client/lib/GameTile.js
+++ client/lib/GameTile.js
@@ -1,97 +1,306 @@
-import {moveSets, boardSize} from './constants.js';
+  import {moveSets, boardSize} from './constants.js';
 
-class GameTile {
-  constructor(name, color, loc, promotedName = null) {
-    this.name = name;
-    this.promotedName = promotedName;
-    this.color = color;
-    this.isPromoted = false;
-    this.canPromote = false;
-    this.loc = loc;
-    this.moves = Array(1);
+  class GameTile {
+    constructor(name, color, loc, promotedName = null) {
+      this.name = name;
+      this.promotedName = promotedName;
+      this.color = color;
+      this.isPromoted = false;
+      this.canPromote = false;
+      this.loc = loc;
+      this.moves = Array(1);
 
-    if (name === "Rook" || name === "Bishop" ) {
-      this.promotedMoves = moveSets[name];
-    } else {
-      this.moves = moveSets[name];
-      if (name !== "King" && name !== "Gold") {
-        this.promotedMoves = moveSets.Gold;
+      if (name === "Rook" || name === "Bishop") {
+        this.promotedMoves = moveSets[name];
+      } else {
+        this.moves = moveSets[name];
+        if (name !== "King" && name !== "Gold") {
+          this.promotedMoves = moveSets.Gold;
+        }
       }
     }
-  }
 
-  findMoves() {
-    let moveSet = this.isPromoted ? this.promotedMoves : this.moves;
+    findMoves(board) {
+      let moveSet = this.isPromoted ? this.promotedMoves : this.moves;
 
-    if (this.name === "Rook") {
-      moveSet = moveSet.concat(this._rookMoves());
-    } else if (this.name === "Bishop") {
-      moveSet = moveSet.concat(this._bishopMoves());
-    } else if (this.name === "Lance" && !this.isPromoted) {
-      moveSet = this._lanceMoves();
-    }
+      if (this.color === "black") {
+        moveSet = moveSet.map(loc => [-loc[0], -loc[1]]);
+      }
 
-    if (this.color === 'black') {
-      moveSet = moveSet.map((loc) => [-loc[0], -loc[1]]);
-    }
+      if (this.name === "Rook") {
+        moveSet = moveSet.concat(this._rookMoves(board));
+      } else if (this.name === "Bishop") {
+        moveSet = moveSet.concat(this._bishopMoves(board));
+      } else if (this.name === "Lance" && !this.isPromoted) {
+        moveSet = this._lanceMoves(board);
+      }
 
-    return (this.name === 'Lance' && !this.isPromoted) ?
-      moveSet.reduce((set, move) => {
-        let position = [this.loc[0] + move[0], this.loc[1]];
-        if (position[0] < boardSize && position[0] >= 0 &&
-          ((position[0] < this.loc[0] && this.color === 'white') ||
-          (postion[0] > this.loc[0] && this.color === 'black'))) {
-            return set.concat([position]);
-          }
+      return moveSet.reduce((set, move) => {
+        let position = [this.loc[0] + move[0], this.loc[1] + move[1]];
+        if (
+          position[0] < boardSize &&
+          position[0] >= 0 &&
+          position[1] < boardSize &&
+          position[1] >= 0 &&
+          !(position[0] === this.loc[0] && position[1] === this.loc[1])
+        ) {
+          return set.concat([position]);
+        }
         return set;
-      }, []) :
-      moveSet.reduce((set, move) => {
-      let position = [this.loc[0] + move[0], this.loc[1] + move[1]];
-      if (position[0] < boardSize && position[0] >= 0 && position[1] < boardSize && position[1] >= 0 && !(position[0] === this.loc[0] && position[1] === this.loc[1])) {
-        return set.concat([position]);
+      }, []);
+    }
+
+    setLocation(loc) {
+      this.loc = loc;
+      if (this.name === "King" || this.name === "Gold") {
+        return;
       }
-      return set;
-    }, []);
-  }
-
-  setLocation(loc) {
-    this.loc = loc;
-    if (this.name === 'King' || this.name === 'Gold') {
-      return;
+      if (
+        (loc[0] < 3 && this.color === "white") ||
+        (loc[0] > boardSize - 4 && this.color === "black")
+      ) {
+        this.canPromote = true;
+      }
     }
-    if ((loc[0] < 3 && this.color === 'white') ||
-      (loc[0] > boardSize - 4 && this.color === 'black')) {
-      this.canPromote = true;
+
+    promote() {
+      this.isPromoted = true;
     }
   }
 
-  promote() {
-    this.isPromoted = true;
-  }
-}
+  GameTile.prototype._rookMoves = function(board) {
+    let result = [];
+    for (let i = 1; i < boardSize; i++) {
+      let position = [this.loc[0] - i, this.loc[1]];
+      if (board[position[0]][position[1]] !== " ") {
+        if (
+          position[0] < 0 ||
+          ((board[position[0]][position[1]].charCodeAt(0) > 90 &&
+            this.color === "white") ||
+            (board[position[0]][position[1]].charCodeAt(0) < 91 &&
+              this.color === "black"))
+        ) {
+          break;
+        } else if (
+          (board[position[0]][position[1]].charCodeAt(0) > 90 &&
+            this.color === "black") ||
+          (board[position[0]][position[1]].charCodeAt(0) < 91 &&
+            this.color === "white")
+        ) {
+          result.push([-i, 0]);
+          break;
+        }
+      } else {
+        result.push([-i, 0]);
+      }
+    }
+    for (let i = 1; i < boardSize; i++) {
+      let position = [this.loc[0] + i, this.loc[1]];
+      if (board[position[0]][position[1]] !== " ") {
+        if (
+          position[0] === boardSize ||
+          ((board[position[0]][position[1]].charCodeAt(0) > 90 &&
+            this.color === "white") ||
+            (board[position[0]][position[1]].charCodeAt(0) < 91 &&
+              this.color === "black"))
+        ) {
+          break;
+        } else if (
+          (board[position[0]][position[1]].charCodeAt(0) > 90 &&
+            this.color === "black") ||
+          (board[position[0]][position[1]].charCodeAt(0) < 91 &&
+            this.color === "white")
+        ) {
+          result.push([i, 0]);
+          break;
+        }
+      } else {
+        result.push([i, 0]);
+      }
+    }
+    for (let i = 1; i < boardSize; i++) {
+      let position = [this.loc[0], this.loc[1] - i];
+      if (board[position[0]][position[1]] !== " ") {
+        if (
+          position[1] < 0 ||
+          ((board[position[0]][position[1]].charCodeAt(0) > 90 &&
+            this.color === "white") ||
+            (board[position[0]][position[1]].charCodeAt(0) < 91 &&
+              this.color === "black"))
+        ) {
+          break;
+        } else if (
+          (board[position[0]][position[1]].charCodeAt(0) > 90 &&
+            this.color === "black") ||
+          (board[position[0]][position[1]].charCodeAt(0) < 91 &&
+            this.color === "white")
+        ) {
+          result.push([0, -i]);
+          break;
+        }
+      } else {
+        result.push([0, -i]);
+      }
+    }
+    for (let i = 1; i < boardSize; i++) {
+      let position = [this.loc[0], this.loc[1] + i];
+      if (board[position[0]][position[1]] !== " ") {
+        if (
+          position[1] + i === boardSize ||
+          ((board[position[0]][position[1]].charCodeAt(0) > 90 &&
+            this.color === "white") ||
+            (board[position[0]][position[1]].charCodeAt(0) < 91 &&
+              this.color === "black"))
+        ) {
+          break;
+        } else if (
+          (board[position[0]][position[1]].charCodeAt(0) > 90 &&
+            this.color === "black") ||
+          (board[position[0]][position[1]].charCodeAt(0) < 91 &&
+            this.color === "white")
+        ) {
+          result.push([0, i]);
+          break;
+        }
+      } else {
+        result.push([0, i]);
+      }
+    }
+    return result;
+  };
 
-GameTile.prototype._rookMoves = () => {
-  let result = [];
-  for (let i = 0; i < boardSize; i++) {
-    result = result.concat([[i, 0], [-i, 0], [0, i], [0, -i]]);
-  }
-  return result;
-};
+  GameTile.prototype._bishopMoves = function(board) {
+    let result = [];
+    for (let i = 1; i < boardSize; i++) {
+      let position = [this.loc[0] - i, this.loc[1] - i];
+      if (board[position[0]][position[1]] !== " ") {
+        if (
+          position[0] < 0 ||
+          position[1] < 0 ||
+          ((board[position[0]][position[1]].charCodeAt(0) > 90 &&
+            this.color === "white") ||
+            (board[position[0]][position[1]].charCodeAt(0) < 91 &&
+              this.color === "black"))
+        ) {
+          break;
+        } else if (
+          (board[position[0]][position[1]].charCodeAt(0) > 90 &&
+            this.color === "black") ||
+          (board[position[0]][position[1]].charCodeAt(0) < 91 &&
+            this.color === "white")
+        ) {
+          result.push([-i, -i]);
+          break;
+        }
+      } else {
+        result.push([-i, -i]);
+      }
+    }
+    for (let i = 1; i < boardSize; i++) {
+      let position = [this.loc[0] + i, this.loc[1] + i];
+      if (board[position[0]][position[1]] !== " ") {
+        if (
+          position[0] === boardSize ||
+          position[1] === boardSize ||
+          ((board[position[0]][position[1]].charCodeAt(0) > 90 &&
+            this.color === "white") ||
+            (board[position[0]][position[1]].charCodeAt(0) < 91 &&
+              this.color === "black"))
+        ) {
+          break;
+        } else if (
+          (board[position[0]][position[1]].charCodeAt(0) > 90 &&
+            this.color === "black") ||
+          (board[position[0]][position[1]].charCodeAt(0) < 91 &&
+            this.color === "white")
+        ) {
+          result.push([i, i]);
+          break;
+        }
+      } else {
+        result.push([i, i]);
+      }
+    }
+    for (let i = 1; i < boardSize; i++) {
+      let position = [this.loc[0] - i, this.loc[1] + i];
+      if (board[position[0]][position[1]] !== " ") {
+        if (
+          position[0] < 0 ||
+          position[1] === boardSize ||
+          ((board[position[0]][position[1]].charCodeAt(0) > 90 &&
+            this.color === "white") ||
+            (board[position[0]][position[1]].charCodeAt(0) < 91 &&
+              this.color === "black"))
+        ) {
+          break;
+        } else if (
+          (board[position[0]][position[1]].charCodeAt(0) > 90 &&
+            this.color === "black") ||
+          (board[position[0]][position[1]].charCodeAt(0) < 91 &&
+            this.color === "white")
+        ) {
+          result.push([-i, i]);
+          break;
+        }
+      } else {
+        result.push([-i, i]);
+      }
+    }
+    for (let i = 1; i < boardSize; i++) {
+      let position = [this.loc[0] + i, this.loc[1] - i];
+      if (board[position[0]][position[1]] !== " ") {
+        if (
+          position[0] < 0 ||
+          position[1] < 0 ||
+          ((board[position[0]][position[1]].charCodeAt(0) > 90 &&
+            this.color === "white") ||
+            (board[position[0]][position[1]].charCodeAt(0) < 91 &&
+              this.color === "black"))
+        ) {
+          break;
+        } else if (
+          (board[position[0]][position[1]].charCodeAt(0) > 90 &&
+            this.color === "black") ||
+          (board[position[0]][position[1]].charCodeAt(0) < 91 &&
+            this.color === "white")
+        ) {
+          result.push([i, -i]);
+          break;
+        }
+      } else {
+        result.push([i, -i]);
+      }
+    }
+    return result;
+  };
 
-GameTile.prototype._bishopMoves = () => {
-  let result = [];
-  for (let i = 0, j = 0; i < boardSize && j < boardSize; [i, j] = [i + 1, j + 1]) {
-    result = result.concat([[i, j], [-i, -j], [-i, j], [i, -j]]);
-  }
-  return result;
-};
+  GameTile.prototype._lanceMoves = function(board) {
+    let result = [];
+    for (let i = 1; i < boardSize; i++) {
+      let position = [this.loc[0] - i, this.loc[1]];
+      if (board[position[0]][position[1]] !== " ") {
+        if (
+          position[0] < 0 ||
+          ((board[position[0]][position[1]].charCodeAt(0) > 90 &&
+            this.color === "white") ||
+            (board[position[0]][position[1]].charCodeAt(0) < 91 &&
+              this.color === "black"))
+        ) {
+          break;
+        } else if (
+          (board[position[0]][position[1]].charCodeAt(0) > 90 &&
+            this.color === "black") ||
+          (board[position[0]][position[1]].charCodeAt(0) < 91 &&
+            this.color === "white")
+        ) {
+          result.push([-i, 0]);
+          break;
+        }
+      } else {
+        result.push([-i, 0]);
+      }
+    }
+    return result;
+  };
 
-GameTile.prototype._lanceMoves = () => {
-  let result = [];
-  for (let i = 1; i < boardSize; i++) {
-    result.push([-i, 0]);
-  }
-  return result;
-}
-
-export default GameTile;
+  export default GameTile;
