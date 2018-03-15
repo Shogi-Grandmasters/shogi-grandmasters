2b10d7d653949edf774d919a4e8a2fd59592d5b6
Aaron Valdez <valdez2010@gmail.com>
2018-03-14 19:28:17 -0700

    Created test component for client socket.

---
+++ client/src/components/LandingPage/socketTest.jsx
@@ -0,0 +1,67 @@
+import React, { Component } from "react";
+import io from "socket.io-client/dist/socket.io.js";
+
+class SocketTest extends Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: [],
+      message: ""
+    };
+  }
+
+  componentWillMount() {
+    this.socket = io("http://localhost:4155", {
+      query: {
+        roomId: "socketTest"
+      }
+    });
+    this.socket.emit("client.fetchMessages");
+  }
+  
+  async componentDidMount() {
+    await this.socket.on("server.gameChat", messages => {
+      messages.length && this.setState({ messages: JSON.parse(messages) });
+    });
+  }
+
+  handleChat(e) {
+    this.setState({ message: e.target.value });
+  }
+
+  async handleSubmit() {
+    const { socket } = this.props;
+    let messages = [...this.state.messages, this.state.message];
+    this.socket.emit("client.gameChat", {
+      messages
+    });
+  }
+
+  resetInput(e) {
+    e.preventDefault();
+    e.target.reset();
+    this.setState({ message: "" });
+  }
+
+  render() {
+    return (
+      <div className="landing-page-container">
+        <h1>Shogi Grandmaster</h1>
+        <form onSubmit={e => this.resetInput(e)}>
+          <textarea onChange={e => this.handleChat(e)} />
+          <button type="submit" onClick={() => this.handleSubmit()}>
+            Submit
+          </button>
+        </form>
+        <div className="chatbox">
+          {this.state.messages.length > 0 &&
+            this.state.messages.map((message, i) => {
+              return <div key={i}>{message}</div>;
+            })}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SocketTest;
