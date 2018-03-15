2d29d65a9e1fb5f3a874bb1b64e2a218bdc8b35d
downier <raulishere@gmail.com>
2018-03-14 17:14:12 -0700

    [auth] Removed extra functions

--- rest-server/src/config/database/setup.js
+++ rest-server/src/config/database/setup.js
@@ -1,10 +1,8 @@
 import Users from "./models/usersModel.js";
-// import { createUserTable } from  "../../lib/SQL";
 
 const setup = async () => {
   await Users.drop();
   await Users.sync();
-  // await createUserTable();
 };
 
 setup();
