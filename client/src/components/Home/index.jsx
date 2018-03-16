583ed6daeaa7d0a4f0b7df7968867dd3be401bfe
Aaron Valdez <valdez2010@gmail.com>
2018-03-15 21:05:15 -0700

    Implemented fetch open matches in home.

--- client/src/components/Home/index.jsx
+++ client/src/components/Home/index.jsx
@@ -1,23 +1,69 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import axios from "axios";
+import randomstring from "randomstring";
+
+import OpenMatches from "../OpenMatches/index.jsx";
 
 class Home extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      openMatches: [],
+      selectedMatch: ""
+    };
+    this.fetchOpenMatches = this.fetchOpenMatches.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchOpenMatches();
   }
   
+  async fetchOpenMatches() {
+    axios
+    .get("http://localhost:3396/api/openmatches")
+    .then(({ data }) => {
+      this.setState({ openMatches: data })
+      });
+  }
+
   logout = () => {
     window.localStorage.clear();
-    this.props.history.push('/login');
+    this.props.history.push("/login");
+  };
+
+  board = JSON.stringify([
+    ["L", "H", "S", "G", "K", "G", "S", "H", "L"],
+    [" ", "R", " ", " ", " ", " ", " ", "B", " "],
+    ["P", "P", "P", "P", "P", "P", "P", "P", "P"],
+    [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+    [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+    ["p", "p", "p", "p", "p", "p", "p", "p", "p"],
+    [" ", "b", " ", " ", " ", " ", " ", "r", " "],
+    ["l", "h", "s", "g", "k", "g", "s", "h", "l"]
+  ]);
+
+  handleInitiateMatchClick() {
+    const matchId = randomstring.generate();
+    const player1 = localStorage.getItem("username");
+    axios
+      .post("http://localhost:3396/api/openmatches", { matchId, player1 })
+      .then(() => this.fetchOpenMatches());
   }
 
   render() {
-    return(
+    return (
       <div>
-        {localStorage.getItem('username')}
+        {localStorage.getItem("username")}
         <button onClick={() => this.logout()}>Logout</button>
+        <br />
+        <button onClick={() => this.handleInitiateMatchClick()}>
+          Initiate Match
+        </button>
+        <OpenMatches openMatches={this.state.openMatches} fetchOpenMatches={this.fetchOpenMatches} />
+        <button onClick={() => this.logout()}>Join Match</button>
       </div>
-    )
+    );
   }
 }
 
