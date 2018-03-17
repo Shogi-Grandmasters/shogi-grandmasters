320d80b8a83c569f0b125dccd9a2531e2a1e4af0
downier <raulishere@gmail.com>
2018-03-17 13:44:39 -0700

    [acct] Saving prior to rebase, working on form validation for search users, add friend

--- rest-server/src/routes/index.js
+++ rest-server/src/routes/index.js
@@ -4,9 +4,14 @@ import authRouter from "../components/auth/authRouter";
 import openMatchesRouter from "../components/openMatches/openMatchesRouter";
 import matchesRouter from "../components/matches/matchesRouter";
 import messagesRouter from "../components/messages/messagesRouter";
+<<<<<<< HEAD
 import userRouter from "../components/users/userRouter"
 import friendsRouter from "../components/friends/friendsRouter";
 import usersRouter from "../components/users/usersRouter";
+=======
+import friendsRouter from "../components/friends/friendsRouter";
+
+>>>>>>> [acct] Saving prior to rebase, working on form validation for search users, add friend
 
 const router = express.Router();
 
@@ -14,8 +19,12 @@ router.use("/auth", authRouter);
 router.use("/openmatches", openMatchesRouter);
 router.use("/matches", matchesRouter);
 router.use("/messages", messagesRouter);
+<<<<<<< HEAD
 router.use("/users", userRouter);
 router.use("/friends", friendsRouter);
 router.use("/users", usersRouter);
+=======
+router.use("/friends", friendsRouter);
+>>>>>>> [acct] Saving prior to rebase, working on form validation for search users, add friend
 
 export default router;
