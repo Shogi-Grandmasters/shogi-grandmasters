317f60d28e175b95e3f65234fc9d8b98c2662263
Paul Valenzano <paul.valenzano@gmail.com>
2018-03-17 12:37:33 -0700

    [game] turn indicators

---
+++ client/src/components/GameBoard/TurnIndicator.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './TurnIndicator.css';
+
+const TurnIndicator = ({ isTurn }) => {
+  let northStyles = ['turn__indicator-north'];
+  let southStyles = ['turn__indicator-south'];
+  isTurn ? southStyles.push('south__active') : northStyles.push('north__active');
+  return (
+    <div className="turn__indicator">
+      <div className={northStyles.join(' ')}></div>
+      <div className="turn__timer">2:00</div>
+      <div className={southStyles.join(' ')}></div>
+    </div>
+  )
+}
+
+export default TurnIndicator;
