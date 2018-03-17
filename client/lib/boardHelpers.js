5d89e1df6c71da76c6f283709c5286350808e98f
Paul Valenzano <paul.valenzano@gmail.com>
2018-03-17 16:34:32 -0700

    [game] bugfix in board helpers (client) and in piece drop logic

--- client/lib/boardHelpers.js
+++ client/lib/boardHelpers.js
@@ -1,7 +1,7 @@
 import {boardIds, oppositeBoardSide, oppositeColor, includesLoc} from './constants.js';
 import GameTile from './GameTile.js';
 
-export const validDropLocations = (tile, board, kings) => {
+export const validDropLocations = (board, kings, tile) => {
   let validDrops = [];
   let pawnLocs = [];
 
@@ -27,7 +27,7 @@ export const validDropLocations = (tile, board, kings) => {
               white: [oppositeBoardSide(kings['white'][0]), oppositeBoardSide(kings['white'][1])],
               black: [oppositeBoardSide(kings['black'][0]), oppositeBoardSide(kings['black'][1])]
             };
-            if(tile.name !== 'Pawn' && !isCheckOrMate(copyMatrix(board), reverseKings, tile.color, new GameTile('Pawn', tile.color, [r, c]))[0]) {
+            if(tile.name !== 'Pawn' && !isCheckOrMate(copyMatrix(board), reverseKings, new GameTile('Pawn', tile.color, [r, c]))[0]) {
               validDrops.push([r, c]);
             }
           }
