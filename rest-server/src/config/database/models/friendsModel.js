27f4bc87bd14f0cd3a90ffe56a76b5c05385076b
downier <raulishere@gmail.com>
2018-03-16 17:29:40 -0700

    [acct] Working on friends model

---
+++ rest-server/src/config/database/models/friendsModel.js
@@ -0,0 +1,35 @@
+import db from "../index";
+import { success, error } from "../../../lib/log";
+
+export const createFriendsTable = async () => {
+  try {
+    await db.queryAsync(
+      `
+      CREATE TABLE IF NOT EXISTS friends
+      (
+        u_id INT NOT NULL,
+        f_id INT NOT NULL
+        status INT NOT NULL,
+        CONSTRAINT friends_pk
+          PRIMARY KEY(id),
+        CONSTRAINT fk_friends_u_id
+          FOREIGN KEY(u_id) REFERENCES users(id),
+        CONSTRAINT fk_friends_f_id
+          FOREIGN KEY(f_id) REFERENCES users(id)
+      )
+      `
+    );
+    success("successfully created friends table");
+  } catch (err) {
+    error("error creating friends table ", err);
+  }
+};
+
+export const dropFriendsTable = async () => {
+  try {
+    await db.queryAsync(`DROP TABLE IF EXISTS friends`);
+    success("successfully dropped friends table");
+  } catch (err) {
+    error("error dropping friends table ", err);
+  }
+};
