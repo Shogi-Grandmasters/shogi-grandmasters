71e045265548b6a60abee43741a865cc1db909a0
Alex Jaynes <dr.ocheyo@gmail.com>
2018-03-17 13:05:22 -0700

    added isCheckOrMate and isValidMove to socket server lib

--- socket-server/lib/constants.js
+++ socket-server/lib/constants.js
@@ -1,13 +1,13 @@
 export const intialBoard = [
-  ['L', 'H', 'S', 'G', 'K', 'G', 'S', 'H', 'L'],
-  [' ', 'R', ' ', ' ', ' ', ' ', ' ', 'B', ' '],
-  ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
-  [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-  [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-  [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-  ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
-  [' ', 'b', ' ', ' ', ' ', ' ', ' ', 'r', ' '],
-  ['l', 'h', 's', 'g', 'k', 'g', 's', 'h', 'l']
+  ["L", "H", "S", "G", "K", "G", "S", "H", "L"],
+  [" ", "R", " ", " ", " ", " ", " ", "B", " "],
+  ["P", "P", "P", "P", "P", "P", "P", "P", "P"],
+  [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+  [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+  [" ", " ", " ", " ", " ", " ", " ", " ", " "],
+  ["p", "p", "p", "p", "p", "p", "p", "p", "p"],
+  [" ", "b", " ", " ", " ", " ", " ", "r", " "],
+  ["l", "h", "s", "g", "k", "g", "s", "h", "l"]
 ];
 
 export const boardIds = {
@@ -22,16 +22,7 @@ export const boardIds = {
 };
 
 export const moveSets = {
-  King: [
-    [-1, -1],
-    [-1, 0],
-    [-1, 1],
-    [0, 1],
-    [1, 1],
-    [1, 0],
-    [1, -1],
-    [0, -1]
-  ],
+  King: [[-1, -1], [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1]],
   Gold: [[-1, -1], [-1, 0], [-1, 1], [0, 1], [1, 0], [0, -1]],
   Silver: [[-1, -1], [-1, 0], [-1, 1], [1, 1], [1, -1]],
   Knight: [[-2, -1], [-2, 1]],
@@ -42,17 +33,18 @@ export const moveSets = {
 
 export const boardSize = 9;
 
-export const oppositeBoardSide = (x) => Math.abs(x - 8);
+export const oppositeBoardSide = x => Math.abs(x - 8);
 
-export const oppositeColor = {
-  white: 'black',
-  black: 'white'
-};
+export const oppositeColor = color => (color === "white" ? "black" : "white");
+
+export const includesLoc = (set, loc) =>
+  set.some(tuple => tuple[0] === loc[0] && tuple[1] === loc[1]);
 
 export default {
   moveSets,
   boardIds,
   boardSize,
   oppositeBoardSide,
-  oppositeColor
+  oppositeColor,
+  includesLoc
 };
