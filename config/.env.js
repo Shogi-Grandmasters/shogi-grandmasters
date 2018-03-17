4da0f9bc2af2d9adc351c39dc62c6af401c006db
Paul Valenzano <paul.valenzano@gmail.com>
2018-03-17 11:58:23 -0700

    docker-compose and buildEnv

---
+++ config/.env.js
@@ -0,0 +1,39 @@
+const envBuild = {
+  "rest-server": [
+    "DEBUG=TRUE",
+    "NODE_ENV=test",
+    "PORT=3396",
+    "LOCAL_USER=postgres",
+    "LOCAL_HOST=localhost",
+    "LOCAL_DATABASE=grandmaster",
+    "LOCAL_PASSWORD=shogiban",
+    "LOCAL_PORT=5432",
+    "AWS_USER=",
+    "AWS_HOST=",
+    "AWS_DATABASE=",
+    "AWS_PASSWORD=",
+    "AWS_PORT=",
+    "SALT_ROUNDS=10",
+    "TOKEN_SECRET=grandmasterapi"
+  ],
+  "socket-server": [
+    "NODE_ENV=DEVELOPMENT",
+    "DEBUG=TRUE",
+    "HOST=http://localhost",
+    "PORT=4155",
+    "REST_SERVER_URL=http://localhost:4990",
+    "TOKEN_SECRET=grandmaster"
+  ],
+  "client/server": ["PORT=1337"],
+  "client": [
+    "NODE_ENV=DEVELOPMENT",
+    "DEBUG=TRUE",
+    "ENVPREFIX=REACT_APP_",
+    "REST_SERVER_URL=http://localhost:4990",
+    "SOCKET_SERVER_URL=http://localhost:4155",
+    "REACT_APP_SOCKET_SERVER_URL=http://localhost:4155",
+    "REACT_APP_REST_SERVER_URL=http://localhost:4990"
+  ]
+};
+
+module.exports = envBuild;
