2f0520d8c0edc56ff7a57fe7a2560a39e835f6d2
Paul Valenzano <paul.valenzano@gmail.com>
2018-03-21 12:37:22 -0700

    [game] css grid layouts, gamechat move

--- client/src/components/GameBoard/PlayerPanel.jsx
+++ client/src/components/GameBoard/PlayerPanel.jsx
@@ -5,10 +5,10 @@ import './PlayerPanel.css';
 const PlayerPanel = ({ player }) => {
   let indicatorStyles = ['player__color', player.color];
   return (
-    <div className="player__profile">
+    <div className={`player__profile-${player.facing}`}>
       <div className={indicatorStyles.join(' ')}></div>
       <div className="player__name">{player.user.name}</div>
-      <div className="player__stats">Rank: 42</div>
+      <div className="player__avatar"></div>
     </div>
   )
 }
