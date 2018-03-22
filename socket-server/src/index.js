0db86a13b42cbde78903d13a98ebde015fa1c22c
Aaron Valdez <valdez2010@gmail.com>
2018-03-22 16:27:22 -0700

    Fixed matches to reload game state on refresh.

--- socket-server/src/index.js
+++ socket-server/src/index.js
@@ -16,14 +16,16 @@ io.on("connection", client => {
   const { roomId, userId, username } = client.handshake.query;
   const room = rooms.findOrCreate(roomId || "default");
   client.join(room.get("id"));
-  client.emit("server.connect", room.get("waiting"));
 
   each(clientEvents, (handler, event) => {
     client.on(event, handler.bind(null, { io, client, room }));
   });
-  
-  const users = room.get("users");
 
+  //reconnecting players to match
+  client.emit("server.reconnect", {black: room.get("black"), white: room.get("white")});
+  
+  //add user to home and set loggedOn status to true
+  const users = room.get("users");
   if (userId && username) {
     if (users) {
       users[userId] = {userId, username, loggedOn: true};
@@ -35,6 +37,7 @@ io.on("connection", client => {
     io.in(roomId).emit("server.userConnected", {userId, username, loggedOn: true});
   }
 
+  //set user loggedOn status to false
   client.on("disconnect", () => {
     users && (users[userId].loggedOn = false, room.set("users", users));
     userId && username && io.in(roomId).emit("server.userDisconnected", {userId, username, loggedOn: false});
