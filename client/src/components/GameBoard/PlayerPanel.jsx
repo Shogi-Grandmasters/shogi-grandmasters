055597b20b97473fc853313dedaa4dc0fc05d3cf
Paul Valenzano <paul.valenzano@gmail.com>
2018-03-24 12:57:43 -0700

    [game] Player Profile ingest for match - pull from feat/friendchallenge - adjusted initializeMatch to trigger on {color}.username - prep'd for avatar use (requires default set instead of null) - defaults of 0 and 0 for Wins and Losses in the User model

--- client/src/components/GameBoard/PlayerPanel.jsx
+++ client/src/components/GameBoard/PlayerPanel.jsx
@@ -3,9 +3,12 @@ import React from 'react';
 import './PlayerPanel.css';
 
 const PlayerPanel = ({ player }) => {
+  let avatarStyles = {
+    backgroundImage: `url(${player.user.avatar})`,
+  }
   return (
     <div className={`player__profile-${player.facing}`}>
-      <div className="player__name">{player.user.name}</div>
+      <div className="player__name">{player.user.username}</div>
       <div className={`player__avatar player__avatar-${player.color}`}></div>
     </div>
   )
