450d4bf66fa7965c8691caf457172efa53251863
Aaron Valdez <valdez2010@gmail.com>
2018-03-26 16:29:04 -0700

    Implemented match play now button.

--- client/src/components/Home/OpenMatches/index.jsx
+++ client/src/components/Home/OpenMatches/index.jsx
@@ -14,76 +14,71 @@ class OpenMatches extends Component {
   }
 
   componentDidMount() {
-    this.fetchOpenMatches();
-    this.props.socket.on("updateOpenMatches", () => {
-      this.fetchOpenMatches();
-    });
+    // this.fetchOpenMatches();
+    // this.props.socket.on("updateOpenMatches", () => {
+    //   this.fetchOpenMatches();
+    // });
   }
 
-  async fetchOpenMatches() {
-    let { data } = await axios.get("http://localhost:3396/api/openmatches");
-    this.setState({ openMatches: data });
-  }
+  // async fetchOpenMatches() {
+  //   let { data } = await axios.get("http://localhost:3396/api/openmatches");
+  //   this.setState({ openMatches: data });
+  // }
 
-  async handleInitiateMatchClick() {
-    let player1 = localStorage.getItem("id");
-    await axios.post("http://localhost:3396/api/openmatches", {
-      matchId: this.matchId,
-      player1
-    });
-    this.props.history.push({
-      pathname: `/match/${this.matchId}`,
-      state: {
-        matchId: this.matchId,
-        black: localStorage.getItem("username"),
-        opponent: false
-      },
-      history: this.props.history
-    });
-    this.props.socket.emit("client.listOpenGames");
-  }
+  // async handleInitiateMatchClick() {
+  //   let player1 = localStorage.getItem("id");
+  //   await axios.post("http://localhost:3396/api/openmatches", {
+  //     matchId: this.matchId,
+  //     player1
+  //   });
+  //   this.props.history.push({
+  //     pathname: `/match/${this.matchId}`,
+  //     state: {
+  //       matchId: this.matchId,
+  //       black: localStorage.getItem("username"),
+  //       opponent: false
+  //     },
+  //     history: this.props.history
+  //   });
+  //   this.props.socket.emit("client.listOpenGames");
+  // }
 
-  async handleMatchSelect(e) {
-    await this.setState({ selectedMatch: JSON.parse(e.target.value) });
-  }
+  // async handleMatchSelect(e) {
+  //   await this.setState({ selectedMatch: JSON.parse(e.target.value) });
+  // }
 
-  matchId = randomstring.generate();
+  // matchId = randomstring.generate();
 
-  async handleJoinMatchClick() {
-    if (this.state.selectedMatch) {
-      let { data } = await axios.delete(
-        "http://localhost:3396/api/openmatches",
-        {
-          data: { matchId: this.state.selectedMatch.id }
-        }
-      );
-      let black = data.player1;
-      this.props.history.push({
-        pathname: `/match/${this.state.selectedMatch.id}`,
-        state: {
-          matchId: this.state.selectedMatch.id,
-          black,
-          white: localStorage.getItem("id")
-        },
-        history: this.props.history
-      });
-    }
-  }
+  // async handleJoinMatchClick() {
+  //   if (this.state.selectedMatch) {
+  //     let { data } = await axios.delete(
+  //       "http://localhost:3396/api/openmatches",
+  //       {
+  //         data: { matchId: this.state.selectedMatch.id }
+  //       }
+  //     );
+  //     let black = data.player1;
+  //     this.props.history.push({
+  //       pathname: `/match/${this.state.selectedMatch.id}`,
+  //       state: {
+  //         matchId: this.state.selectedMatch.id,
+  //         black,
+  //         white: localStorage.getItem("id")
+  //       },
+  //       history: this.props.history
+  //     });
+  //   }
+  // }
 
   async handlePlayMatchClick() {
     let player1 = localStorage.getItem("username");
     this.props.history.push({
       pathname: '/match/queue',
       state: {
-        matchId: this.matchId,
-        black: localStorage.getItem("username"),
-        opponent: false
+        userId: localStorage.getItem("id"),
+        opponent: false,
       },
       history: this.props.history
-    });
-    this.props.socket.emit("client.playMatch", {
-      matchId: this.matchId,
-      username: localStorage.getItem("username")
     });
   }
 
@@ -91,29 +86,7 @@ class OpenMatches extends Component {
     return (
       <div className="open_matches">
         <div className="match_actions">
-          <h3>Open Matches</h3>
           <button onClick={() => this.handlePlayMatchClick()}>Play Now</button>
-          <button onClick={() => this.handleInitiateMatchClick()}>
-            Initiate Match
-          </button>
-        </div>
-        <div className="match_select">
-          <select onChange={e => this.handleMatchSelect(e)} size="20">
-            <option className="match__items">Select a Match</option>
-            {this.state.openMatches &&
-              this.state.openMatches.map(match => {
-                return (
-                  <option key={match.id} value={JSON.stringify(match)}>
-                    {match.username}
-                  </option>
-                );
-              })}
-          </select>
-        </div>
-        <div className="match_actions">
-          <button onClick={() => this.handleJoinMatchClick()}>
-            Join Match
-          </button>
         </div>
       </div>
     );
