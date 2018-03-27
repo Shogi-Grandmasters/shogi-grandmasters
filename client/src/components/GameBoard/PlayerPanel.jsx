751ba9910b9fe272cfebb25022019ebeb6759e8e
Paul Valenzano <paul.valenzano@gmail.com>
2018-03-27 15:50:35 -0700

    [game] match-end endpoint, win/loss updates on users

--- client/src/components/GameBoard/PlayerPanel.jsx
+++ client/src/components/GameBoard/PlayerPanel.jsx
@@ -9,7 +9,7 @@ const PlayerPanel = ({ player }) => {
   return (
     <div className={`player__profile-${player.facing}`}>
       <div className="player__name">{player.user.username}</div>
-      <div className={`player__avatar player__avatar-${player.color}`}></div>
+      <div className={`player__avatar player__avatar-${player.color}`} style={avatarStyles}></div>
     </div>
   )
 }
