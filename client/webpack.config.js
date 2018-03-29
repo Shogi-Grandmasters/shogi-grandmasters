a3fe428c11caeefb2e9eaed670f9a028d7736ae2
Alex Jaynes <dr.ocheyo@gmail.com>
2018-03-29 09:02:14 -0700

    deploy headaches

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -44,7 +44,7 @@ module.exports = {
         }
       },
       {
-        test: /\.(css)$/,
+        test: /\.(scss|css)$/,
         loaders: ["style-loader", "css-loader", "postcss-loader"]
       },
       {
