7b075d920e1ef03f6842188d252873443821986e
downier <raulishere@gmail.com>
2018-03-31 15:01:00 -0700

    [acct] Some components styled

--- client/src/components/Account/MatchHistory.jsx
+++ client/src/components/Account/MatchHistory.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import duel from '../../../public/5fb83b603cb5c95c8cbdffb9cb379888.png'
+import './Account.css';
 
 const {REST_SERVER_URL, AVATAR_URL} = process.env;
 
@@ -22,15 +23,14 @@ class MatchHistory extends Component {
     let { data } = await axios.get(`${REST_SERVER_URL}/api/matches/history/`, {
       params: { id: id }
     });
-    console.log('our fetch match', data)
     for (let match of data) {
       const fid = match.black == id ? match.white : match.black;
       const user = await axios.get(`${REST_SERVER_URL}/api/users/${fid}`);
       match.opponentId = user.data[0].id
       match.outcome = match.winner == id ? 'Win' : 'Loss'
       match.opponentName = user.data[0].username
-      console.log(user.data[0])
       match.opponentAvi = user.data[0].avatar
+      match.opponentRank = user.data[0].rating_ranked
       match.status != 0 && history.push(match);
     }
     this.setState({history: history});
@@ -38,20 +38,29 @@ class MatchHistory extends Component {
 
   render() {
     return (
-      <div className="online-container"> 
-        <div className="online-head">Match History:</div>
-        <div className="online-list-container">
-        {this.state.history.map((match, index) => (
-          <div className="online-friend-container" key={index} >
-            <img width="50px" className="friend-avi" src={`${AVATAR_URL}${localStorage.avi}`} />
-            {localStorage.username}
-            <b className="online-username">{match.outcome}</b>
-            <b className="online-username">VS</b>
-            <img width="50px" className="friend-avi" src={`${AVATAR_URL}${match.opponentAvi}`} />
-            {match.opponentName}
+      <div className="history-container">
+      <div className="history-list-container"> 
+        <div className="history-head">Match History:</div>
+        <div className="history-list">
+          <div className="history-list-inner">
+          {this.state.history.map((match, index) => (
+            <div className="history-item" key={index} >
+              <div className="history-user-container">
+              <img width="50px" className="friend-avi" src={`${AVATAR_URL}${localStorage.avi}`} />
+              {localStorage.username}
+              </div>
+              <div classname="history-outcome-container" >
+              <h1>{match.outcome}</h1>
+              </div>
+              <div className="history-opponent-container">
+              <img width="50px" className="friend-avi" src={`${AVATAR_URL}${match.opponentAvi}`} />
+              {match.opponentName}
+              </div>
+            </div>
+          ))}
           </div>
-        ))}
         </div>
+      </div>
       </div>
     );
   }
