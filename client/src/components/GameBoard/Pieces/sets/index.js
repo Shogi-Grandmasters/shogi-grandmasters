e6babe8638433a0e463a906e644a0273c0cf6f54
Paul Valenzano <paul.valenzano@gmail.com>
2018-04-03 07:34:59 -0700

    [game] reworked SVGs, traditional glyphs

---
+++ client/src/components/GameBoard/Pieces/sets/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import Traditional from './traditional';
+
+export default {
+  Traditional,
+}
---
+++ client/src/components/GameBoard/Pieces/sets/palettes.js
@@ -0,0 +1,18 @@
+const palettes = {
+  white: {
+    fill: '#FFF',
+    stroke: '#DDD',
+    strokeWidth: '2px',
+    text: '#000',
+    promoted: '#f00',
+  },
+  black: {
+    fill: '#FFF',
+    stroke: '#ececec',
+    strokeWidth: '2px',
+    text: '#000',
+    promoted: '#f00',
+  }
+}
+
+export default palettes;
