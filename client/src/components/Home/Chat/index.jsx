be296ec1e9639f39ac879e25c4e9f1f71d939f15
Aaron Valdez <valdez2010@gmail.com>
2018-04-03 20:56:09 -0700

    Added chat popups.

--- client/src/components/Home/Chat/index.jsx
+++ client/src/components/Home/Chat/index.jsx
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import io from "socket.io-client/dist/socket.io.js";
 import { unescape } from "lodash";
 
+import './Chat.css';
+
 class HomeChat extends Component {
   constructor(props) {
     super(props);
@@ -11,17 +13,21 @@ class HomeChat extends Component {
     };
   }
 
-  componentWillMount() {
-    this.props.socket.emit("client.fetchMessages");
-  }
+  componentWillMount() {}
 
   async componentDidMount() {
+    this.props.socket.emit("client.fetchMessages");
+
     await this.props.socket.on("server.homeChat", message => {
       this.setState({ messages: [message, ...this.state.messages] });
     });
     await this.props.socket.on("server.sendMessages", messages => {
       this.setState({ messages });
     });
+  }
+
+  componentWillUnmount() {
+    this.props.socket.close();
   }
 
   async handleChat(e) {
@@ -51,21 +57,31 @@ class HomeChat extends Component {
   }
 
   render() {
+    let chatStyles = ['match__chat'];
+    this.props.visibility ? chatStyles.push('shown') : chatStyles.push('hidden');
+
     return (
-      <div className="home-chat-container">
-        <form onSubmit={e => this.resetInput(e)}>
-          <textarea onKeyUp={e => this.handleChat(e)} />
-          {/* <button type="submit" onClick={() => this.handleSubmit()}>
-            Submit
-          </button> */}
-        </form>
-        <div className="chatbox">
-          {this.state.messages.length > 0 &&
-            this.state.messages.slice(0, 20).map((message, i) => {
-              return (
-                <div key={i}>{`${message.username}: ${unescape(message.content)}`}</div>
-              );
-            })}
+      <div>
+        <div className="match__chat-container">
+          <div className="chatbox">
+            {this.state.messages.length > 0 &&
+              this.state.messages.slice(0, 20).map((message, i) => {
+                return (
+                  <div className="chat__message" key={i}><strong>{message.username}</strong> {unescape(message.content)}</div>
+                );
+              })}
+          </div>
+          <form onSubmit={e => this.resetInput(e)}>
+            <div className="chat__input">
+              <input onKeyUp={e => this.handleChat(e)} />
+              <button type="submit" onClick={() => this.handleSubmit()}>
+                &gt;
+              </button>
+            </div>
+          </form>
+        </div>
+        <div className="match__chat-actions">
+          <button className="chat__close" onClick={() => this.toggle('chat')}>Close</button>
         </div>
       </div>
     );
