5e97fa25fdc942c9ab6b626c1e48a697e13d02ce
Paul Valenzano <paul.valenzano@gmail.com>
2018-04-06 09:26:20 -0700

    transitions, form styles

--- client/src/components/Auth/Signup.jsx
+++ client/src/components/Auth/Signup.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { HalfLockup } from '../Global/Logo/index.js'
+import { FadeIn } from '../Global/Animation/Transitions.jsx';
 
 import './Auth.css';
 
@@ -33,44 +34,48 @@ class Signup extends Component {
 
   render() {
     return (
-      <div className="auth__container">
-        <div className="logo__container">
-          <HalfLockup />
-        </div>
-        <form className="auth__form-container">
-          <input
-            name='email'
-            type='text'
-            className='auth-form'
-            placeholder={'ENTER YOUR EMAIL'}
-            autoComplete='email'
-            onChange={this.handleInputChange}
-            />
-          <input
-            name='username'
-            type='text'
-            className='auth-form'
-            autoComplete="username"
-            placeholder={'ENTER YOUR USERNAME'}
-            onChange={this.handleInputChange}
-            />
-          <input
-            name='password'
-            type='password'
-            className='auth-form'
-            autoComplete="new-password"
-            placeholder={'ENTER YOUR PASSWORD'}
-            onChange={this.handleInputChange}
-            />
-          <button
-            type='submit'
-            className='auth__button'
-            onClick={(e) => this.submitAuthData(e)}
-            >Submit</button>
-        </form>
-        <div className="landing__signup-cta">
-          <em>Already have an account?</em> <a className="landing__link-secondary" onClick={() => this.props.history.push("/login")}>Login</a>
-        </div>
+      <div className="auth__page">
+        <FadeIn>
+          <div className="auth__page-container">
+            <div className="logo__container">
+              <HalfLockup />
+            </div>
+            <form className="auth__form-container">
+              <input
+                name='email'
+                type='text'
+                className='auth__form-input'
+                placeholder={'ENTER YOUR EMAIL'}
+                autoComplete='email'
+                onChange={this.handleInputChange}
+                />
+              <input
+                name='username'
+                type='text'
+                className='auth__form-input'
+                autoComplete="username"
+                placeholder={'ENTER YOUR USERNAME'}
+                onChange={this.handleInputChange}
+                />
+              <input
+                name='password'
+                type='password'
+                className='auth__form-input'
+                autoComplete="new-password"
+                placeholder={'ENTER YOUR PASSWORD'}
+                onChange={this.handleInputChange}
+                />
+              <button
+                type='submit'
+                className='auth__button'
+                onClick={(e) => this.submitAuthData(e)}
+                >Submit</button>
+            </form>
+            <div className="landing__signup-cta">
+              <em>Already have an account?</em> <a className="landing__link-secondary" onClick={() => this.props.history.push("/login")}>Login</a>
+            </div>
+          </div>
+        </FadeIn>
       </div>
     )
   }
