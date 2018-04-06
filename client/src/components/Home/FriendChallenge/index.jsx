4d016135e301adc33073ba5a2b1f68538912cba1
ocheyo <dr.ocheyo@gmail.com>
2018-04-06 19:41:17 +0000

    removed public

--- client/src/components/Home/FriendChallenge/index.jsx
+++ client/src/components/Home/FriendChallenge/index.jsx
@@ -6,7 +6,7 @@ import randomstring from 'randomstring';
 import './FriendChallenge.css';
 import duel from '../../../../5fb83b603cb5c95c8cbdffb9cb379888.png';
 import scroll from '../../../../58df55f617483f263a3c2880d16ce947.png';
-import awaitingDuel from '../../../../public/6277b9a543e1dc34eb2e3e99b42af758.png';
+import awaitingDuel from '../../../../6277b9a543e1dc34eb2e3e99b42af758.png';
 
 const { REST_SERVER_URL, AVATAR_URL } = process.env;
 
