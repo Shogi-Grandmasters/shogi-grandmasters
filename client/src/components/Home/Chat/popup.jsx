717a2b45f87aa2f50f703931902dee9ed323fdbe
Paul Valenzano <paul.valenzano@gmail.com>
2018-04-05 17:21:47 -0700

    popup integrated, general responsiveness updates

--- client/src/components/Home/Chat/popup.jsx
+++ client/src/components/Home/Chat/popup.jsx
@@ -66,7 +66,7 @@ class ChatPopup extends Component {
   setTimestamps() {
     let { timestamps, messages } = this.state;
     for (let message of messages) {
-      !timestamps[message.user_id] && message.user_id !== this.id 
+      !timestamps[message.user_id] && message.user_id !== this.id
         && (timestamps[message.user_id] = message.created);
     }
     this.setState({ timestamps });
@@ -74,7 +74,7 @@ class ChatPopup extends Component {
 
   renderTimestamps(id) {
     const { timestamps } = this.state;
-    return timestamps[id] 
+    return timestamps[id]
       ? <div className="timestamp">{moment(timestamps[id]).fromNow()}</div> : null;
   }
 
@@ -106,9 +106,11 @@ class ChatPopup extends Component {
                     &#8212;
                   </a>
                 )}
-                <a onClick={() => this.props.removeActivePopup(friend.id)}>
-                  &#10006;
-                </a>
+                {this.props.removeActivePopup ? (
+                  <a onClick={() => this.props.removeActivePopup(friend.id)}>
+                    &#10006;
+                  </a>
+                ) : null}
               </div>
             </div>
             <div
